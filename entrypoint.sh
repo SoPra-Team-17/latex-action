35183a68b9209d175bcd9443ba4e2a940f097047
Jonas Otto <jottosmail@gmail.com>
2019-12-12 14:32:51 +0100

    change to build one top-level document

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,42 +9,17 @@ set -e
 echo "Creating deploy directory: $DEPLOY_DIR"
 mkdir -p "$DEPLOY_DIR"
 
-echo "<!DOCTYPE HTML>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>$GITHUB_REPOSITORY</title>
-</head>
-<body>
-    <h1>$GITHUB_REPOSITORY</h1>
-    <ul>" > $DEPLOY_DIR/index.html
+cd $REPO_DIR
 
-for directory in */; do
+if [[ ! -f "Makefile" ]]; then
+    echo "No Makefile found."
+    continue
+fi
 
-    if [[ $directory == *"build/" ]]; then
-        continue
-    fi
+ARTIFACTS=$(make | grep "$ARTIFACT_PREFIX")
+ARTIFACTS=${ARTIFACTS#"$ARTIFACT_PREFIX"}
+echo "Build artifacts: $ARTIFACTS"
 
-    echo "Building $directory"
-    cd $REPO_DIR/$directory
-
-    if [[ ! -f "Makefile" ]]; then
-        echo "No Makefile found."
-        continue
-    fi
-
-    ARTIFACTS=$(make | grep "$ARTIFACT_PREFIX")
-    ARTIFACTS=${ARTIFACTS#"$ARTIFACT_PREFIX"}
-    echo "Build artifacts: $ARTIFACTS"
-
-    for artifact in $(echo $ARTIFACTS | sed "s/,/ /g"); do
-        mkdir -p $(dirname "$DEPLOY_DIR/$directory/$artifact")
-        cp "$REPO_DIR/$directory/$artifact" "$DEPLOY_DIR/$directory/$artifact"
-        echo "        <li><a href=\"$directory$artifact\">$directory$artifact</a></li>" >> $DEPLOY_DIR/index.html
-    done
-    
+for artifact in $(echo $ARTIFACTS | sed "s/,/ /g"); do
+    cp "$REPO_DIR/$artifact" "$DEPLOY_DIR/$artifact"
 done
-
-echo "    </ul>
-</body>
-</html>" >> $DEPLOY_DIR/index.html
